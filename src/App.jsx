077ee2303e80a2098e9aa39173cf70e12e493698
Amit-1807 <150581997+Amit-1807@users.noreply.github.com>
2025-08-03 17:04:16 +0530

    1

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import Background from './components/background/Background'
+import Navbar from './components/navbar/Navbar'
+
+function App() {
+let heroData =[
+  {text1: "Dive into" , text2: "what you love"},
+  {text1: "Indulge" , text2: "your passions"},
+  {text1: "Give in to" , text2: "your passions"}
+]
+const [heroCount,setHeroCount] = useState(0)
+const [playStatus,setPlayStatus] = useState(false)
+
+  return (
+    <>
+    <Background playStatus={playStatus} heroCount={heroCount}/>
+    <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,8 @@
+import './Hero.css'
+const Hero = () => {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
